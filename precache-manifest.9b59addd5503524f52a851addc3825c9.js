f68355bd918aed9dc818b10adec0680d1cddacf6
Pin Sern <universityforfang@gmail.com>
2020-06-18 18:45:24 +0800

    Updates

---
+++ precache-manifest.9b59addd5503524f52a851addc3825c9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3044bc0bb5914c445bc95bf5bc928ec",
+    "url": "/PersonalResumeWebsite/index.html"
+  },
+  {
+    "revision": "5b1c1606588efb020fb2",
+    "url": "/PersonalResumeWebsite/static/css/4.3abaee18.chunk.css"
+  },
+  {
+    "revision": "9f2f33f61e15a987fe2e",
+    "url": "/PersonalResumeWebsite/static/css/5.c082522e.chunk.css"
+  },
+  {
+    "revision": "890d842700ee7c34ba1e",
+    "url": "/PersonalResumeWebsite/static/css/main.5d909151.chunk.css"
+  },
+  {
+    "revision": "eb0e577c02a79bdab9f1",
+    "url": "/PersonalResumeWebsite/static/js/2.0dc66d4f.chunk.js"
+  },
+  {
+    "revision": "fbefcf69c239393b56c6",
+    "url": "/PersonalResumeWebsite/static/js/3.08c2b668.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PersonalResumeWebsite/static/js/3.08c2b668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1c1606588efb020fb2",
+    "url": "/PersonalResumeWebsite/static/js/4.5ba3d1d8.chunk.js"
+  },
+  {
+    "revision": "9f2f33f61e15a987fe2e",
+    "url": "/PersonalResumeWebsite/static/js/5.8d5deb83.chunk.js"
+  },
+  {
+    "revision": "890d842700ee7c34ba1e",
+    "url": "/PersonalResumeWebsite/static/js/main.c5731e7a.chunk.js"
+  },
+  {
+    "revision": "e36d4560cf603adff27c",
+    "url": "/PersonalResumeWebsite/static/js/runtime-main.0699b73b.js"
+  },
+  {
+    "revision": "dc25c7a9d6747d8de770634499dda625",
+    "url": "/PersonalResumeWebsite/static/media/aboutPageImage.dc25c7a9.jpg"
+  },
+  {
+    "revision": "c1286f3ab6a930ca00a4f33117d3013b",
+    "url": "/PersonalResumeWebsite/static/media/headerImage.c1286f3a.jpg"
+  },
+  {
+    "revision": "85f85648f28221b56a7aa7a49514e937",
+    "url": "/PersonalResumeWebsite/static/media/resumeImage.85f85648.jpg"
+  },
+  {
+    "revision": "912af16af7fc5877a6bf50851fd62f9d",
+    "url": "/PersonalResumeWebsite/static/media/square1Image.912af16a.jpg"
+  },
+  {
+    "revision": "f358e58630adcf4b8a7d68644f072844",
+    "url": "/PersonalResumeWebsite/static/media/square2Image.f358e586.jpg"
+  },
+  {
+    "revision": "012ed890aeed92ccb840384380a451c5",
+    "url": "/PersonalResumeWebsite/static/media/square3Image.012ed890.jpg"
+  }
+]);
