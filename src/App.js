7effabc8c1412eee6ee7383334e87dae39d0a782
Pin Sern <universityforfang@gmail.com>
2020-06-19 12:28:05 +0800

    Added general config to customize website title.\
    - Webpage title is not customizable

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Home from "./home/pages/Home";
 import MainNavigation from "./shared/Navigation/MainNavigation";
 import LoadingSpinner from "./shared/modals/LoadingSpinner";
 
 import "./App.css";
+import { generalConfig } from "./config/generalConfig";
 
 // In case you do not want to lazy load
 // import About from "./about/pages/About";
@@ -22,6 +24,8 @@ const About = React.lazy(() => import("./about/pages/About"));
 const Contact = React.lazy(() => import("./contact/pages/Contact"));
 
 function App() {
+  const websiteTitle = generalConfig.websiteTitle;
+
   let routes = (
     <Switch>
       <Route path="/" exact>
@@ -38,20 +42,25 @@ function App() {
   );
 
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Suspense
-          fallback={
-            <div className="center">
-              <LoadingSpinner />
-            </div>
-          }
-        >
-          {routes}
-        </Suspense>
-      </main>
-    </Router>
+    <React.Fragment>
+      <Helmet>
+        <title>{websiteTitle}</title>
+      </Helmet>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/config/generalConfig.js
@@ -0,0 +1,3 @@
+export const generalConfig = {
+  websiteTitle: "Title", //Name that appears on the left of the navbar
+};
